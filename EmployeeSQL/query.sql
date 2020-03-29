6aeb5cef8fd3686f249bc21e6ac88feba730ae36
Etumi <etumi12@gmail.com>
2020-03-29 15:34:51 -0400

    query code for last two questions

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -61,3 +61,38 @@ ON e.emp_no = dept_emp.emp_no
 WHERE d.dept_name = 'Sales'
 ORDER BY emp_no
 ;
+
+
+---7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+---use first code if department name not needed
+SELECT emp_no, last_name, first_name
+FROM "employees" AS e
+WHERE emp_no IN(
+	SELECT emp_no
+	FROM "dept_emp"
+	WHERE dept_emp.dept_no IN(
+		SELECT dept_no
+		FROM "departments"
+		WHERE dept_name = 'Sales' OR dept_name = 'Development'
+	)
+)
+ORDER BY emp_no
+;
+---use code below if department name needed
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM "employees" AS e
+JOIN "dept_emp"
+ON e.emp_no = dept_emp.emp_no
+	JOIN "departments" AS d
+	ON dept_emp.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' OR dept_name = 'Development'
+ORDER BY emp_no
+;
+
+
+---8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(emp_no) AS "Frequency"  
+FROM "employees"
+GROUP BY last_name
+ORDER BY last_name DESC
+;
