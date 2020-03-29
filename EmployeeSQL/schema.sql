22f06d7f09d407e8ba221261fffdb3197609378e
Etumi <etumi12@gmail.com>
2020-03-29 14:24:08 -0400

    change all date variables from string to date

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -15,11 +15,11 @@ CREATE TABLE "departments" (
 
 CREATE TABLE "employees" (
     "emp_no" INTEGER   NOT NULL,
-    "birth_date" VARCHAR(255)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
     "first_name" VARCHAR(255)   NOT NULL,
     "last_name" VARCHAR(255)   NOT NULL,
     "gender" VARCHAR(255)   NOT NULL,
-    "hire_date" VARCHAR(255)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
@@ -28,8 +28,8 @@ CREATE TABLE "employees" (
 CREATE TABLE "dept_emp" (
     "emp_no" INTEGER   NOT NULL,
     "dept_no" VARCHAR(255)   NOT NULL,
-    "from_date" VARCHAR(255)   NOT NULL,
-    "to_date" VARCHAR(255)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE  NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "emp_no","dept_no"
      )
@@ -38,8 +38,8 @@ CREATE TABLE "dept_emp" (
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(255)   NOT NULL,
     "emp_no" INTEGER   NOT NULL,
-    "from_date" VARCHAR(255)   NOT NULL,
-    "to_date" VARCHAR(255)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY (
         "dept_no","emp_no"
      )
@@ -48,8 +48,8 @@ CREATE TABLE "dept_manager" (
 CREATE TABLE "salaries" (
     "emp_no" INTEGER   NOT NULL,
     "salary" INTEGER   NOT NULL,
-    "from_date" VARCHAR(255)   NOT NULL,
-    "to_date" VARCHAR(255)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
     CONSTRAINT "pk_salaries" PRIMARY KEY (
         "emp_no"
      )
@@ -58,8 +58,8 @@ CREATE TABLE "salaries" (
 CREATE TABLE "titles" (
     "emp_no" INTEGER   NOT NULL,
     "title" VARCHAR(255)   NOT NULL,
-    "from_date" VARCHAR(255)   NOT NULL,
-    "to_date" VARCHAR(255)   NOT NULL
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
 );
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
